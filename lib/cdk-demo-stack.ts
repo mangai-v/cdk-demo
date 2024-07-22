884b9144ed7099cb785ae6f1d1baa173c78f0aed
Your Name <you@example.com>
2024-07-22 17:31:43 +0530

    First Commit

--- lib/cdk-demo-stack.ts
+++ lib/cdk-demo-stack.ts
@@ -1,6 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkDemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -9,8 +9,8 @@ export class CdkDemoStack extends cdk.Stack {
     // The code that defines your stack goes here
 
     // example resource
-    // const queue = new sqs.Queue(this, 'CdkDemoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const queue = new sqs.Queue(this, 'CdkDemoQueue', {
+      queueName:"mangayarkkarasivd"
+    });
   }
 }
